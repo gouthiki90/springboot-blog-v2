c56816726b64365b3bfdbe8f151faf806e6c7363
전가은 <jeonga-eun@jeonga-eun-ui-MacBookAir.local>
2022-03-22 12:35:10 +0900

    4. 회원가입 완료

--- src/main/java/site/metacoding/blogv2/service/UserService.java
+++ src/main/java/site/metacoding/blogv2/service/UserService.java
@@ -1,13 +1,22 @@
 package site.metacoding.blogv2.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.domain.user.UserRepository;
+import site.metacoding.blogv2.web.api.dto.user.JoinDto;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
-    
+
     private final UserRepository userRepository;
+
+    @Transactional
+    public void 회원가입(JoinDto joinDto) {
+        // save하면 DB에 insert하고 결과를 다시 return해준다.
+        // 받을 필요가 없기 때문에 void로 한다.
+        userRepository.save(joinDto.toEntity());
+    }
 }
--- src/main/java/site/metacoding/blogv2/web/UserController.java
+++ src/main/java/site/metacoding/blogv2/web/UserController.java
@@ -1,13 +1,28 @@
 package site.metacoding.blogv2.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.service.UserService;
+import site.metacoding.blogv2.web.api.dto.user.JoinDto;
 
 @RequiredArgsConstructor
 @Controller
 public class UserController {
     
     private final UserService userService;
+    
+    // 웹브라우저 회원가입 페이지 O, 앱은 X swing으로 줘야 함
+    @GetMapping("/join") // 페이지를 달라
+    public String joinForm(@RequestBody JoinDto joinDto){
+        userService.회원가입(joinDto);
+        return "user/joinForm";
+    }
+
+    @GetMapping("/login") // 페이지를 달라
+    public String loginForm(){
+        return "user/loginForm";
+    }
 }
